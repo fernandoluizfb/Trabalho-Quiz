fdc3822d271438e2e0cc9712b8bdf6bd760b7c8e
fernandoluizfb <99805892+fernandoluizfb@users.noreply.github.com>
2022-05-20 19:16:23 -0300

    Update Javaquiz.js

--- Javaquiz.js
+++ Javaquiz.js
@@ -18,34 +18,29 @@ function checkAnswer( event ) {
       // Troca o texto do resultado por “Acertou :)”
       resultado.textContent = 'Se você ama futebol e não conhece Cláudio, isso não é exatamente um problema. É que o maior artilheiro de um dos maiores clubes do Brasil se tornou famoso na década de 1950. Em 550 jogos, ele marcou 305 gols e levantou 8 taças'
   
-      // Aplica cor de “acertou”
       resultado.style.color = 'White'
-  
-      // Mostra gatinho
+
       image.style.display = 'claudio.jpg'
   
     } else {
   
- 
       resultado.textContent = 'Errou :('
-  
-      // Aplica cor de “errou”
-      result.style.color = 'DeepPink'
+
+      result.style.color = 'Red'
   
     }
-  
-    // Mostra .answer
+
     answer.style.display = 'initial'
   
   }
   
-  // Seleciona todas as <li>
+
   let alternativas = document.querySelectorAll( 'li' )
   
-  // Para cada <li>
+
   for ( let alternativa of alternativas ) {
   
-    // Ouve o evento de click e roda a função
+
     alternativa.addEventListener( 'click', checkAnswer )
   
   }
